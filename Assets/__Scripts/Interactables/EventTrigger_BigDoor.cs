ec9c7ae1c65d68c9a5d6484a5ea457dbbb411ac2
FerWar6 <91885869+FerWar6@users.noreply.github.com>
2024-04-08 00:08:14 +0200

    new enemy fighting area/gameplay loop

--- Assets/__Scripts/Interactables/EventTrigger_BigDoor.cs
+++ Assets/__Scripts/Interactables/EventTrigger_BigDoor.cs
@@ -6,6 +6,13 @@ public class EventTrigger_BigDoor : MonoBehaviour
     [SerializeField]
     private Animator anim;
 
+    [SerializeField]
+    private EnemySpawner spawner;
+
+    [SerializeField]
+    private GameObject enemy1;
+    [SerializeField]
+    private GameObject enemy2;
     private void OnTriggerExit(Collider other)
     {
         if (other.CompareTag("Player"))
@@ -16,11 +23,17 @@ public class EventTrigger_BigDoor : MonoBehaviour
 
     private IEnumerator TriggerEventWithDelay()
     {
-        yield return new WaitForSeconds(3f);
+        yield return new WaitForSeconds(0.1f);
 
         if (anim != null)
         {
             anim.SetTrigger("CloseDoor");
+            if(spawner != null)
+            {
+                spawner.startWaves = true;
+                Destroy(enemy1);
+                Destroy(enemy2);
+            }
             Destroy(gameObject);
         }
         else
